a074162eb88a18fab8298f841d28597c8ca3cc4e
Shuvam Shah <shvmks@outlook.com>
2020-07-19 03:18:40 +0530

    Smoothing function 4 added

--- bleuper/function.py
+++ bleuper/function.py
@@ -1,34 +1,38 @@
 import abc
+import math
 from fractions import Fraction
-from typing import Dict, Union
+from typing import Dict, Union, List
+from .sentence import Sentence
+
 
 class Function(metaclass=abc.ABCMeta):
   """
   An interface for all smoothing functions
   """
+  
   @classmethod
   def __subclasshook__(cls, subclass):
     return hasattr(subclass, 'compute') and callable(subclass.compute)
   
-  def __init__(self, epsilon, alpha, k):
+  def __init__(self, epsilon):
     self.epsilon = epsilon
-    self.alpha = alpha
-    self.k = k
   
   @abc.abstractmethod
   def compute(self, precisions: Dict[int, Fraction]) -> Dict[int, Union[float, Fraction]]:
     return NotImplemented
 
+
 class SmoothFunc1(Function):
   """
   if n-gram overlaps correspond to zero, it resets them to epsilon
   """
+  
   def compute(self, precisions: Dict[int, Fraction]) -> Dict[int, Union[float, Fraction]]:
     return {
       k: (v.numerator + self.epsilon) / v.denominator
       for k, v in precisions.items()
     }
-    
+
 
 class SmoothFunc2(Function):
   def compute(self, precisions: Dict[int, Fraction]) -> Dict[int, Union[float, Fraction]]:
@@ -38,14 +42,38 @@ class SmoothFunc2(Function):
       for k, v in precisions.items()
     }
 
+
 class SmoothFunc3(Function):
   def compute(self, precisions: Dict[int, Fraction]) -> Dict[int, Union[float, Fraction]]:
-    invert = 1
+    invcnt = 1
     newPrecisions = dict()
     for k, v in precisions.items():
       if v.numerator == 0:
-        newPrecisions[k] = 1 / (v.denominator * (1 << invert))
-        invert += 1
+        newPrecisions[k] = 1 / (v.denominator * (1 << invcnt))
+        invcnt += 1
+      else:
+        newPrecisions[k] = v
+    
+    return newPrecisions
+
+
+class SmoothFunc4(Function):
+  def __init__(self, epsilon, k, trans: Sentence):
+    super().__init__(epsilon)
+    assert len(trans) == 0, "Provided translated sentence length has length 0."
+    
+    self.k = k
+    self.trans = trans
+  
+  def compute(self, precisions: Dict[int, Fraction]) -> Dict[int, Union[float, Fraction]]:
+    invcnt = 1
+    factor = math.log(k / len(self.trans))
+    newPrecisions = dict()
+    
+    for k, v in precisions.items():
+      if v.numerator == 0:
+        invcnt *= factor
+        newPrecisions[k] = 1 / (v.denominator * invcnt)
       else:
         newPrecisions[k] = v
     
